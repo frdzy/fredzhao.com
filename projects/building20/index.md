b380f41e85e0949c381e608fd3dffa0df9a58b23
Fred Zhao <fred.y.zhao@gmail.com>
2016-02-16 16:54:06 -0800

    Generate Projects with webpack build script

--- projects/building20/index.md
+++ projects/building20/index.md
@@ -1,9 +1,8 @@
 ---
 layout: default
 customjs:
- - build/react-with-addons-0.13.1.js
- - build/photo_data.js
- - build/building20.js
+ - build/core/commons.js
+ - build/projects/building20.js
 ---
 
 <div id="build20-canvas"></div>
---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+var CommonsChunkPlugin = require('webpack/lib/optimize/CommonsChunkPlugin');
+
+var jsRoot = '/public/js';
+
+var projects = [
+  'building20',
+];
+
+function getEntries(projectNames) {
+  var entries = {};
+  projectNames.forEach(function(projectName) {
+    entries['/projects/' + projectName] =
+      __dirname + jsRoot + '/src/projects/' + projectName + '/main.js';
+  });
+  return entries;
+}
+
+var projectNames = [
+  'building20',
+];
+
+module.exports = {
+  entry: getEntries(projectNames),
+  output: {
+    path: __dirname + jsRoot + '/build',
+    filename: '[name].js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015', 'react'],
+        },
+        exclude: /node_modules/
+      },
+      {
+        test: /\.json$/,
+        loader: 'json',
+        exclude: /node_modules/
+      }
+    ],
+  },
+  plugins: [
+    new CommonsChunkPlugin('core/commons.js', projectNames),
+  ],
+};
