55bcb1570186237c734d68502f4f2f63d52d95b3
Fred Zhao <fred.y.zhao@gmail.com>
2017-09-20 22:06:59 -0700

    New blog post: All the Threads We Cannot See

---
+++ public/js/src/projects/allthethreads/main.js
@@ -0,0 +1,16 @@
+var React = require('react');
+
+var Canvas = React.createClass({
+  render: function() {
+    return (
+      <div>
+        App goes here
+      </div>
+    );
+  },
+});
+
+React.render(
+  <Canvas />,
+  document.getElementById('allthethreads')
+);
